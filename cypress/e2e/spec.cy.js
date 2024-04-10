c3426b707f932b2108c6e7a3b559b3a36c21bb69
Steven Ni <steven.christopher.ni@gmail.com>
2024-04-10 09:00:05 -0600

    Added end-to-end test to verify navigation from main page to specific Pokemon page and check for correct content.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,3 +6,12 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Visit Ivysaur', function () {
+  it('can visit a specific pokemon page', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
+  })
+})
