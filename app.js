590985cb970386cc671739d38939303660dc5b06
Steven Ni <steven.christopher.ni@gmail.com>
2024-04-17 12:45:43 -0600

    Simulated server failure by listening on incorrect port for testing health check endpoint.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  this_causes_error
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
